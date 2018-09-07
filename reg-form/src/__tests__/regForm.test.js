115126baf7ed4349be4858e4d7e6558c6ae6489d
Dmytro Snishchuk <ddemarkk@gmail.com>
2018-09-07 22:21:58 +0300

    Updated to React 16.5.0

---
+++ reg-form/src/__tests__/regForm.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+import RegForm from '../components/reg-form';
+import Select from '../components/reg-form';
+
+
+it('displays correctly', () => {
+    const wrapper = shallow(<RegForm />)
+    expect(wrapper).toMatchSnapshot();
+})
+it("state is correct ", () => {
+    const wrapper = shallow(<RegForm />)
+    expect(wrapper.state().postCode).toEqual("")
+})
+it("placeholder correct ", () => {
+    const wrapper = shallow(<RegForm />)
+
+    expect(wrapper.find('Select').shallow().hasClass('placeholder')).toEqual(true)
+
+    //expect(wrapper.state().postCode).toEqual('35816')
+})
+it("Sign Up is visible ", () => {
+    const wrapper = shallow(<RegForm />)
+    wrapper.find('input.check').simulate('click');
+    expect(wrapper.contains(<button className="butt">
+        Sign Up
+  </button >)).toEqual(true)
+
+    //expect(wrapper.state().postCode).toEqual('35816')
+})
+
+it("phone number mask displays", () => {
+    const wrapper = shallow(<RegForm />)
+    wrapper.update()
+    expect(wrapper.contains('logo')).toEqual(true)
+
+})
